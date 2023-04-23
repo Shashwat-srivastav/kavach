3694bea14fbb454a277b33e5f3127e65781a5db6
Shashwat srivastava <37861820+Shashwat-srivastav@users.noreply.github.com>
2023-04-23 17:56:22 +0530

    almost done

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,15 @@
+import 'dart:async';
 import 'dart:convert';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:suraksha_kavach/gaurdianinfo.dart';
+import 'package:twilio_flutter/twilio_flutter.dart';
 import "package:velocity_x/velocity_x.dart";
 import 'gaurdianinfo.dart';
+import 'package:qr_flutter/qr_flutter.dart';
 
 void main() {
   runApp(const MyApp());
@@ -23,6 +27,60 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.purple,
       ),
       home: const LoginPage(),
+    );
+  }
+}
+
+//----------------------------------------------------------------------------------------------------------
+//QR code
+class QRGenerator extends StatelessWidget {
+  final String s;
+  const QRGenerator({super.key, required this.s});
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+        child: Scaffold(
+            body: Container(
+      child: InkWell(
+        child: Center(
+          child: QrImage(
+            data: s,
+            size: 320,
+            version: QrVersions.auto,
+            gapless: false,
+          ),
+        ),
+      ),
+    )));
+  }
+}
+
+//------------------------------------------------------------------------------------------------------------
+//Google Map
+const String API_KEY = "AIzaSyD_6M5LwdYq96HnHD28WcQ6Zgfx1dd6nd8";
+const LatLng destination = LatLng(28.535517, 77.391029);
+
+class Direction extends StatefulWidget {
+  const Direction({super.key});
+
+  @override
+  State<Direction> createState() => _DirectionState();
+}
+
+class _DirectionState extends State<Direction> {
+  final Completer<GoogleMapController> _controller = Completer();
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Scaffold(
+          body: GoogleMap(
+        initialCameraPosition: CameraPosition(target: destination),
+        markers: {
+          Marker(markerId: MarkerId("destination"), position: destination)
+        },
+      )),
     );
   }
 }
@@ -91,10 +149,18 @@ class _PoliceStationState extends State<PoliceStation> {
                                   h.url.text.make()
                                 ],
                               ),
-                              Icon(
-                                color: Colors.blueGrey,
-                                CupertinoIcons.arrow_right_square,
-                                size: 50,
+                              InkWell(
+                                onTap: () {
+                                  Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) => Direction()));
+                                },
+                                child: Icon(
+                                  color: Colors.blueGrey,
+                                  CupertinoIcons.arrow_right_square,
+                                  size: 50,
+                                ),
                               )
                             ],
                           )).h(80),
@@ -281,6 +347,46 @@ class _GaurdianState extends State<Gaurdian> {
   }
 }
 
+//twilio integration
+class SendSmsTwilio extends StatefulWidget {
+  final String title;
+  const SendSmsTwilio({super.key, required this.title});
+
+  @override
+  State<SendSmsTwilio> createState() => _SendSmsTwilioState();
+}
+
+class _SendSmsTwilioState extends State<SendSmsTwilio> {
+  late TwilioFlutter twilio;
+  @override
+  void initState() {
+    twilio = TwilioFlutter(
+        accountSid: 'AC459301fb0a89bc2c978967e16ad2490c',
+        authToken: 'd6011acdf30ef1aefc09173a2b334d77',
+        twilioNumber: '+16813956194');
+    super.initState();
+  }
+
+  void sendSms() async {
+    twilio.sendSMS(
+        toNumber: '+919450702570',
+        messageBody: 'Hii everyone this our team stealth squad.');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: InkWell(
+        child: Icon(CupertinoIcons.arrow_right),
+        onTap: () {
+          sendSms();
+        },
+      ),
+    );
+  }
+}
+//response page
+
 //--------------------------------------------------------------------------------------------------------------
 //Profile
 class Profile extends StatelessWidget {
@@ -289,81 +395,101 @@ class Profile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Scaffold(
-        backgroundColor: Colors.pink[50],
-        // appBar: AppBar(),
-        body: SafeArea(
-          child: Column(mainAxisAlignment: MainAxisAlignment.start, children: [
-            Placeholder().h10(context).w20(context),
-            InkWell(
-              onTap: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => Gaurdian()));
-              },
-              child: Card(
-                elevation: 10,
-                margin: EdgeInsets.fromLTRB(20, 0, 10, 0),
-                color: Color.fromARGB(255, 231, 197, 236),
-                child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      Placeholder().h10(context).w10(context).p12(),
-                      "Guardian".text.headline4(context).gray500.make().p16()
-                    ]),
-              ).h(100).w(500),
-            ).pLTRB(0, 20, 0, 0),
-            InkWell(
-              onTap: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => PoliceStation()));
-              },
-              child: Card(
-                elevation: 10,
-                margin: EdgeInsets.fromLTRB(20, 0, 10, 0),
-                color: Color.fromARGB(255, 231, 197, 236),
-                child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      Placeholder().h10(context).w10(context).p12(),
-                      "Police".text.headline4(context).gray500.make().p16()
-                    ]),
-              ).h(100).w(500),
-            ).pLTRB(0, 20, 0, 0),
-            InkWell(
-              onTap: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => HospitalList()));
-              },
-              child: Card(
-                elevation: 10,
-                margin: EdgeInsets.fromLTRB(20, 0, 10, 0),
-                color: Color.fromARGB(255, 231, 197, 236),
-                child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      Placeholder().h10(context).w10(context).p12(),
-                      "Hospital".text.headline4(context).gray500.make().p16()
-                    ]),
-              ).h(100).w(500),
-            ).pLTRB(0, 20, 0, 0),
-          ]).pLTRB(0, 50, 0, 0),
-        ),
+      child: InkWell(
+        onDoubleTap: () {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => SendSmsTwilio(title: "helo")));
+        },
+        child: Scaffold(
+          floatingActionButton: FloatingActionButton(
+              child: Icon(CupertinoIcons.qrcode),
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => QRGenerator(
+                            s: "Our team name is stealth squad" +
+                                "hello " +
+                                n)));
+              }),
+          backgroundColor: Colors.pink[50],
+          // appBar: AppBar(),
+          body: SafeArea(
+            child:
+                Column(mainAxisAlignment: MainAxisAlignment.start, children: [
+              Placeholder().h10(context).w20(context),
+              InkWell(
+                onTap: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => Gaurdian()));
+                },
+                child: Card(
+                  elevation: 10,
+                  margin: EdgeInsets.fromLTRB(20, 0, 10, 0),
+                  color: Color.fromARGB(255, 231, 197, 236),
+                  child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Placeholder().h10(context).w10(context).p12(),
+                        "Guardian".text.headline4(context).gray500.make().p16()
+                      ]),
+                ).h(100).w(500),
+              ).pLTRB(0, 20, 0, 0),
+              InkWell(
+                onTap: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => PoliceStation()));
+                },
+                child: Card(
+                  elevation: 10,
+                  margin: EdgeInsets.fromLTRB(20, 0, 10, 0),
+                  color: Color.fromARGB(255, 231, 197, 236),
+                  child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Placeholder().h10(context).w10(context).p12(),
+                        "Police".text.headline4(context).gray500.make().p16()
+                      ]),
+                ).h(100).w(500),
+              ).pLTRB(0, 20, 0, 0),
+              InkWell(
+                onTap: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => HospitalList()));
+                },
+                child: Card(
+                  elevation: 10,
+                  margin: EdgeInsets.fromLTRB(20, 0, 10, 0),
+                  color: Color.fromARGB(255, 231, 197, 236),
+                  child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Placeholder().h10(context).w10(context).p12(),
+                        "Hospital".text.headline4(context).gray500.make().p16()
+                      ]),
+                ).h(100).w(500),
+              ).pLTRB(0, 20, 0, 0),
+            ]).pLTRB(0, 50, 0, 0),
+          ),
 
-        // drawer: SafeArea(
-        //   child: Container(
-        //     margin: EdgeInsets.fromLTRB(20, 0, 10, 0),
-        //     color: Color.fromARGB(255, 231, 197, 236),
-        //     child: Column(
-        //       children: ["Gaurdian".text.make()],
-        //     ),
-        //   ),
-        // ),
+          // drawer: SafeArea(
+          //   child: Container(
+          //     margin: EdgeInsets.fromLTRB(20, 0, 10, 0),
+          //     color: Color.fromARGB(255, 231, 197, 236),
+          //     child: Column(
+          //       children: ["Gaurdian".text.make()],
+          //     ),
+          //   ),
+          // ),
+        ),
       ),
     );
   }
 }
 
-//-----------------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------------------------
 //Gaurdian creation
 class GaurdianCreate extends StatelessWidget {
   const GaurdianCreate({super.key});
@@ -423,6 +549,7 @@ class GaurdianCreate extends StatelessWidget {
   }
 }
 
+//-------------------------------------------------------------------------------------------------------
 //medical info
 class Medical extends StatelessWidget {
   const Medical({super.key});
@@ -491,6 +618,7 @@ class Medical extends StatelessWidget {
   }
 }
 
+//------------------------------------------------------------------------------------------------------------
 //SignUp
 class SignUp extends StatelessWidget {
   const SignUp({super.key});
@@ -551,7 +679,10 @@ class SignUp extends StatelessWidget {
   }
 }
 
+//-----------------------------------------------------------------------------------------------------
 //login page
+String n = "";
+
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
 
@@ -573,6 +704,9 @@ class LoginPage extends StatelessWidget {
               color: Color.fromARGB(255, 231, 197, 236),
               child: Column(children: [
                 TextFormField(
+                        onChanged: (value) {
+                          n = value;
+                        },
                         decoration: InputDecoration(
                             hintText: "ID",
                             border: OutlineInputBorder(
@@ -616,6 +750,7 @@ class LoginPage extends StatelessWidget {
   }
 }
 
+//------------------------------------------------------------------------------------------------------------------
 //Disclaimer page
 class Disclaimer extends StatelessWidget {
   const Disclaimer({super.key});
@@ -626,35 +761,44 @@ class Disclaimer extends StatelessWidget {
       child: Scaffold(
         backgroundColor: Colors.pink[50],
         body: SafeArea(
-          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-            "Disclaimer".text.capitalize.bold.center.headline3(context).make(),
-            Card(
-              // child: Placeholder().h(550).w(400),
-              elevation: 20,
-              child:
-                  "Follow Indian cyber security laws and regulations: Make sure to comply with all Indian cyber security laws and regulations when developing Android applications. These include the Indian IT Act, which provides legal recognition to electronic documents and digital signatures, and the Cyber Crime Investigation Manual, which provides guidelines for investigating cyber crimes."
-                      .text
-                      .headline6(context)
-                      .center
-                      .fade
-                      .make()
-                      .p(20),
-              color: Color.fromARGB(255, 231, 197, 236),
-              shadowColor: Color.fromARGB(255, 221, 8, 79),
-              // margin: EdgeInsets.fromLTRB(20, 100, 20, 60),
-              borderOnForeground: true,
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10)),
-            ).h(600).pLTRB(10, 40, 10, 0),
-            ElevatedButton(
-                    onPressed: () {
-                      Navigator.push(context,
-                          MaterialPageRoute(builder: (context) => Profile()));
-                    },
-                    child: Text("Next"))
-                .w(70)
-                .p(50)
-          ]),
+          child: SingleChildScrollView(
+            child:
+                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+              "Disclaimer"
+                  .text
+                  .capitalize
+                  .bold
+                  .center
+                  .headline3(context)
+                  .make(),
+              Card(
+                // child: Placeholder().h(550).w(400),
+                elevation: 20,
+                child:
+                    "Follow Indian cyber security laws and regulations: Make sure to comply with all Indian cyber security laws and regulations when developing Android applications. These include the Indian IT Act, which provides legal recognition to electronic documents and digital signatures, and the Cyber Crime Investigation Manual, which provides guidelines for investigating cyber crimes."
+                        .text
+                        .headline6(context)
+                        .center
+                        .fade
+                        .make()
+                        .p(20),
+                color: Color.fromARGB(255, 231, 197, 236),
+                shadowColor: Color.fromARGB(255, 221, 8, 79),
+                // margin: EdgeInsets.fromLTRB(20, 100, 20, 60),
+                borderOnForeground: true,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10)),
+              ).h(400).pLTRB(10, 40, 10, 0),
+              ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(context,
+                            MaterialPageRoute(builder: (context) => Profile()));
+                      },
+                      child: Text("Next"))
+                  .w(70)
+                  .p(50)
+            ]),
+          ),
         ),
       ),
     );
